da57f99a4e7521cacbdeeae5a89aab267c8f134d
Jonathan Green <jonathangreen@Jonathans-iMac.fios-router.home>
2017-09-22 08:57:51 -0700

    WIP

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 let package = Package(
     name: "Shout",
     dependencies: [
-        .Package(url: "https://github.com/b20160/CSSH", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/BlueSocket", majorVersion: 0)
     ]
 )
